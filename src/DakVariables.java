2a5f9e5fb8fe1407e6a9861db79eaa6ae9d540f3
IssyyA <isiomaandrew@gmail.com>
2020-04-16 21:59:19 +0200

    Initial commit

---
+++ src/DakVariables.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class DakVariables {
+    //This Class contains all variables
+    static double A1 = 0.3265;
+    static double A2 = -1.0700;
+    static double A3 = -0.5339;
+    static double A4 = 0.01569;
+    static double A5 = -0.05165;
+    static double A6 = 0.5475;
+    static double A7 = -0.7361;
+    static double A8 = 0.1844;
+    static double A9 = 0.1056;
+    static double A10 = 0.6134;
+    static double A11 = 0.7210;
+    static double specificGravity = 0.64;
+    static double specificGravity2 = Math.pow(0.64, 2);
+    static double pcPressure = (4.6 + (0.1 * specificGravity) - (0.258 * specificGravity2)) * 10.1325 * 14.7;
+    static double pcTemp = (99.3 + (180 * specificGravity) - (6.94 * specificGravity2)) * 1.8;
+
+    static double pPR;
+    static double tPR;
+    double c1;
+    double c2;
+    double c3;
+
+    public void computerVariables(){
+        //Input value of Pressure
+        Scanner input = new Scanner(System.in);
+    // Input value of Pressure
+     System.out.print("Input the Pressure: ");
+        double pressure = input.nextDouble();
+    //Input value of Temperature
+     System.out.print("Input the Temperature: ");
+        double temperature = input.nextDouble();
+
+        double pPR = (pressure / pcPressure);
+        //System.out.println(pPR);
+
+        double tPR = (temperature / pcTemp);
+        // System.out.println(tPR);
+
+        double c1 = (A1 + (A2 / tPR) + (A3 / Math.pow(tPR, 3)) + (A4 / Math.pow(tPR, 4)) + (A5 / Math.pow(tPR, 5)));
+        //System.out.println(c1);
+
+        double c2 = (A6 + (A7 / tPR) + (A8 / Math.pow(tPR, 2)));
+        // System.out.println(c2);
+
+        double c3 = (A9 * (A7 / tPR) + (A8 / Math.pow(tPR, 2)));
+        //  System.out.println(c3);
+
+    }
+
+      // This calculates for the variable pr ie Reduced density
+    public double reducedDensity(double Ppr , double Tpr, double z){
+        double d = ((0.27 * Ppr)/(z * Tpr));
+        return d;
+    }
+
+    // This calculates for the variable c4
+    public double newtonVariable4(double pr,double tPR, double z){
+        double e1 = ((2 * A10 * Math.pow(pr, 2))/ (z * Math.pow(tPR, 3)));
+        double g1 = A11 * Math.pow(pr,2);
+        double g2 = Math.pow(A11* Math.pow(pr,2),2);
+        double g3 = Math.exp((-A11) * Math.pow(pr,2));
+        double d4 = e1 *(1+g1-g2)*3;
+        return d4;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/NewtonsMethod.java
@@ -0,0 +1,97 @@
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+/**
+ * f(z1) = z1- (1 + (c1 * pr) + (c2 * Math.pow(pr,2) - (c3 * Math.pow(pr,5) + c4)
+ * */
+
+public class NewtonsMethod extends DakVariables {
+
+//    static double pr;
+    private double z;
+
+    public NewtonsMethod(double z){
+        this.z = z;
+    }
+
+
+    public static double newtonPrime(double c1, double c2, double c3, double c4, double pr, double z, double A11, double A10, double tPR){
+        //This method computes for f'(zi)
+        double d1 = ((c1 * pr)/z);
+        double d2 = ((2*c2*Math.pow(pr,2))/z);
+        double d3 = ((5*c3*Math.pow(pr,5))/z);
+        double e1 = ((2 * A10 * Math.pow(pr, 2))/ (z * Math.pow(tPR, 3)));
+        double g1 = A11 * Math.pow(pr,2);
+        double g2 = Math.pow(A11* Math.pow(pr,2),2);
+        double g3 = Math.exp((-A11) * Math.pow(pr,2));
+        double d4 = e1 *(1+g1-g2)*3;
+
+        double result = 1 + d1 +d2 -d3 + d4;
+        return result;
+    }
+    public double newtonFunct(double c1, double c2, double c3, double c4, double pr, double z){
+        //This method computes for f(zi)
+        double d1 = z;
+        double d2 = c1*pr;
+        double d3 = c2*Math.pow(pr,2);
+        double d4 = (-c3) * Math.pow(pr, 5);
+        double d5 = c4;
+
+        double result = d1 - (1 + d2 + d3 - d4 + d5);
+        return result;
+    }
+    public static double newtonRaphsonEq(double funct1, double funct2, double z){
+        double d1 = z;
+        double d2 = (funct1/funct2);
+        double result = d1 - d2;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+
+// Taking the initial guess of z to be 0.5 for Newton-Raphson method
+        NewtonsMethod dark = new NewtonsMethod(0.5);
+        dark.computerVariables();
+        double pr = dark.reducedDensity(dark.pPR , dark.tPR, dark.z);
+        double c4 = dark.newtonVariable4(pr,dark.tPR,dark.z);
+
+        int count = 0;
+        double x3 = 0;
+        while(count < 200){
+            //infinite iteration .No condition yet ,get the object of the class
+            System.out.println("Iteration "+count);
+            if(count > 0) {
+                dark.z = 0.5;
+                pr = dark.reducedDensity(dark.pPR, dark.tPR, dark.z);
+                c4 = dark.newtonVariable4(pr, dark.tPR, dark.z);
+            }
+//
+
+            double x2 = dark.newtonPrime(dark.c1,dark.c2,dark.c3,c4,pr,dark.z,dark.A11,dark.A10,dark.tPR);
+            double x1 = dark.newtonFunct(dark.c1,dark.c2,dark.c3,c4,pr,dark.z);
+            x3 =  newtonRaphsonEq(x1, x2, dark.z);
+            System.out.println("Output of Iteration is "+ x3);
+            count++;
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
